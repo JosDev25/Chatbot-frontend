dd70613338988e054db00335fe66c7adc5b2f605
JosDev25 <joseph.rodriguez@valere.io>
2025-03-04 11:38:28 -0500

    #2 feat(ChatBox): Added input validation and response paragraph

--- src/modules/chat/components/ChatBox.tsx
+++ src/modules/chat/components/ChatBox.tsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+import { fetchChatResponse } from "../../auth/services/api";
 
 const ChatBox = () => {
-  return (
-    <>
-      Welcome to Freemiun-GPT
-      
-    </>
-  )
-}
+    const [input, setInput] = useState("");
+    const [response, setResponse] = useState("");
+    const [error, setError] = useState("")
 
-export default ChatBox
+    const handleInputChange = (e: { target: { value: string; }; }) => {
+        const value = e.target.value;
+        setInput(value);
+
+        if (value.length < 10) {
+            setError("Ingresa al menos 10 carácteres");
+        } else {
+            setError("")
+        }
+    }
+
+    const handleSend = async () => {
+        if (error) {
+            alert("Por favor, corrige el error antes de enviar.");
+            return;
+        }
+        if (input.length < 10) {
+            alert("Por favor, ingresa al menos 10 caracteres");
+            return;
+        }
+        const reply = await fetchChatResponse(input);
+        setResponse(reply);
+    };
+
+
+    return (
+        <>
+            <h1>Freemiun GPT</h1>
+            <input value={input} onChange={handleInputChange} placeholder="Escribe algo..." />
+            {error && <h3 style={{ color: "red" }}>{error}</h3>}
+            <button onClick={handleSend}>Enviar</button>
+            <p>Respuesta: {response}</p>
+        </>
+    );
+};
+
+export default ChatBox;

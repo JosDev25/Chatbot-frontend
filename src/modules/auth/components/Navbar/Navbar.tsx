02253d8aabcb144c8d2683490da95d3063902e21
JosDev25 <joseph.rodriguez@valere.io>
2025-03-06 09:02:21 -0500

    #14 refactor(Navbar): Added onClick to show register and login

--- src/modules/auth/components/Navbar/Navbar.tsx
+++ src/modules/auth/components/Navbar/Navbar.tsx
@@ -1,17 +1,25 @@
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
 
-const Navbar = () => {
+interface NavbarProps {
+  onLoginClick: () => void;
+  onRegisterClick: () => void;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ onLoginClick, onRegisterClick }) => {
   return (
     <nav className="navbar">
       <div className="navbar-title">
         Freemiun-GPT 🤖
       </div>
       <div className="navbar-links">
-        <a href="/" className="navbar-link">Home</a> {/*Will be updated with routing*/}
-        {/* <a href="/about" className="navbar-link">About</a>
-        <a href="/contact" className="navbar-link">Contact</a> */}
-        <button className="navbar-button navbar-login">Login</button>
-        <button className="navbar-button navbar-signup">Signup</button>
+        <a href="/" className="navbar-link">Home</a>
+        <button className="navbar-button navbar-login" onClick={onLoginClick}>
+          Login
+        </button>
+        <button className="navbar-button navbar-signup" onClick={onRegisterClick}>
+          Signup
+        </button>
       </div>
     </nav>
   );

21c0b63718687a15f4fbc83ff82cb5e59b76e328
JosDev25 <joseph.rodriguez@valere.io>
2025-03-06 16:46:57 -0500

    #20 feat(Register): Added register function to send POST method with axios

--- src/modules/auth/services/api.ts
+++ src/modules/auth/services/api.ts
@@ -12,9 +12,9 @@ export const LoginValidation = async (email: string, password: string): Promise<
         if (data.success) {
             console.log("Éxito en el login");
 
-            if (data.user?.name && data.user?.email) { 
+            if (data.user?.name && data.user?.email) {
                 console.log("Usuario cargado correctamente:", data.user);
-                return `Bienvenido, ${data.user.name}!`;  
+                return `Bienvenido, ${data.user.name}!`;
             } else {
                 console.error("Error: La respuesta no tiene datos completos.");
                 return "Error: Datos de usuario incompletos.";
@@ -28,3 +28,23 @@ export const LoginValidation = async (email: string, password: string): Promise<
         return "Error al conectar con el servidor.";
     }
 };
+
+export const Register = async (name: string, email: string, password: string): Promise<string> => {
+    try {
+        const response = await axios.post("http://localhost:8000/register", {
+            name: name,
+            email: email,
+            password: password
+        })
+
+        return response.data;
+    } catch (error) {
+        console.error("Error al registrar", error)
+        if (axios.isAxiosError(error) && error.response) {
+            return `Error: ${error.response.data.message || "Error desconocido"}`;
+        } else {
+            return "Error de conexión con el servidor.";
+        }
+
+    }
+}

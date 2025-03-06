6ae82f826b50a3e906d407ab6f74278939b29762
JosDev25 <joseph.rodriguez@valere.io>
2025-03-06 12:30:16 -0500

    #17 fix(LoginForm): Now handles submit and onChange events from api

--- src/modules/auth/components/LoginForm.tsx
+++ src/modules/auth/components/LoginForm.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ModalForm.css'
+import { LoginValidation } from '../services/api';
 
 interface LoginFormProps {
-    isOpen: boolean;
-    onClose: () => void;
+    isOpen: boolean
+    onClose: () => void
 }
 
 const LoginForm: React.FC<LoginFormProps> = ({ isOpen, onClose }) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [message, setMessage] = useState<string | null>(null)
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault()
+
+        const responsMessage = await LoginValidation(email, password)
+        setMessage(responsMessage)
+    }
+
     if (!isOpen) return null;
 
     return (
@@ -17,17 +29,30 @@ const LoginForm: React.FC<LoginFormProps> = ({ isOpen, onClose }) => {
                 </button>
                 <div className="modal-form">
                     <h2>Iniciar Sesión</h2>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <label>Email</label>
                         <br />
-                        <input type="email" name="email" className="modal-input" />
+                        <input
+                            type="email"
+                            name="email"
+                            className="modal-input"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)} />
                         <br />
                         <label>Password</label>
                         <br />
-                        <input type="password" name="password" className="modal-input" />
+                        <input
+                            type="password"
+                            name="password"
+                            className="modal-input"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
                         <br />
-                        <button type="submit" className="modal-button">Iniciar Sesión</button>
+                        <button
+                            type="submit"
+                            className="modal-button">Iniciar Sesión</button>
                     </form>
+                    {message && <p className="modal-message">{message}</p>}
                 </div>
             </div>
         </div>

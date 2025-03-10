95192b5e0fcdbd974fb6f25e1bc0f0c3c3224899
JosDev25 <joseph.rodriguez@valere.io>
2025-03-10 14:50:02 -0500

    #36 style(LoginForm): Added br tag to create input space

--- src/modules/auth/components/LoginForm.tsx
+++ src/modules/auth/components/LoginForm.tsx
@@ -17,13 +17,13 @@ const LoginForm: React.FC<LoginFormProps> = ({ isOpen, onClose, setIsLoggedIn, s
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
         const response = await LoginValidation(email, password);
-        
+
         if (response.startsWith("Bienvenido")) {
             const name = response.split(",")[1].trim();
             setIsLoggedIn(true);
             setUserName(name);
             localStorage.setItem("user", JSON.stringify({ name, email }));
-            onClose();  
+            onClose();
         } else {
             setMessage(response);
         }
@@ -41,19 +41,23 @@ const LoginForm: React.FC<LoginFormProps> = ({ isOpen, onClose, setIsLoggedIn, s
                     <h2>Iniciar Sesión</h2>
                     <form onSubmit={handleSubmit}>
                         <label>Email</label>
-                        <input 
-                            type="email" 
+                        <br />
+                        <input
+                            type="email"
                             className="modal-input"
                             value={email}
-                            onChange={(e) => setEmail(e.target.value)} 
+                            onChange={(e) => setEmail(e.target.value)}
                         />
+                        <br />
                         <label>Password</label>
-                        <input 
-                            type="password" 
+                        <br />
+                        <input
+                            type="password"
                             className="modal-input"
                             value={password}
-                            onChange={(e) => setPassword(e.target.value)} 
+                            onChange={(e) => setPassword(e.target.value)}
                         />
+                        <br />
                         <button type="submit" className="modal-button">
                             Iniciar Sesión
                         </button>
